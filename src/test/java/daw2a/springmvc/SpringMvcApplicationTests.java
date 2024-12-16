07deeae2e9cf295fc953d55df4d8963c11c6b469
marta <msanchezl@iesrafaelalberti.es>
2024-12-16 12:26:07 +0100

    first commit

---
+++ src/test/java/daw2a/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package daw2a.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
